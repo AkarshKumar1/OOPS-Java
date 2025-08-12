015b801a3cad5fc70da1747df635ae31d7bf876e
AkarshKumar1 <knot14hx8@gmail.com>
2025-08-12 10:23:42 +0530

    Add files via upload

---
+++ Code11.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Code11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking user input
+        System.out.print("Enter the number of terms: ");
+        int n = sc.nextInt();
+
+        // First two terms of Fibonacci series
+        int first = 0, second = 1;
+
+        System.out.print("Fibonacci Series: ");
+
+        // Loop to generate the Fibonacci series
+        for (int i = 1; i <= n; i++) {
+            System.out.print(first + " ");
+
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+}
