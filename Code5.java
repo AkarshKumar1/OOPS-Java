016b70c5d749cd27ca2e3c4a9a90b64718fd9f1b
Akarsh Kumar <knot14hx8@gmail.com>
2025-08-19 11:57:48 +0530

    W.A.P to print address

---
+++ Code5.java
@@ -0,0 +1,7 @@
+// W.A.P to print address (LAB Practical) {Code2}
+
+public class Code5 {
+    public static void main(String[] args) {
+        System.out.println("B-117, P.C. Colony, Kankarbagh, Patna");
+}
+}
