c79c7f1588d5ce14b2aa0263eaee2dfe58fca3c9
Akarsh Kumar <knot14hx8@gmail.com>
2025-08-29 11:42:12 +0530

    W.A.P to calculate the length of any no. input by user

---
+++ Length_of_a_number.java
@@ -0,0 +1,22 @@
+//W.A.P to calculate the length of any no. input by user  
+
+import java.util.Scanner;
+public class Lengthofanumber {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        // Taking input from user
+        System.out.print("Enter a number: ");
+        long num = sc.nextLong();
+
+        // Handling negative numbers
+        num = Math.abs(num);
+
+        // Convert to string and get length
+        int length = String.valueOf(num).length();
+
+        System.out.println("The length of the number is: " + length);
+
+        sc.close();
+    }
+}
