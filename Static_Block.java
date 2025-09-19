89bd2c3daffbdfef702596534b90300253710363
AkarshKumar1 <knot14hx8@gmail.com>
2025-09-19 11:24:23 +0530

    Create Static_Block.java

---
+++ Static_Block.java
@@ -0,0 +1,8 @@
+public class Static_Block {
+    static {
+        System.out.println("Static Block Initialized.");
+    }
+    public static void main(String[] args) {
+        System.out.println("from main");
+    }
+}
