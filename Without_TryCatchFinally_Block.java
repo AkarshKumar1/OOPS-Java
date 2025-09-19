91132ab64a786d3c26fe4781692d13c65fe7b603
AkarshKumar1 <knot14hx8@gmail.com>
2025-09-19 11:24:13 +0530

    Exception Handelling

---
+++ Without_TryCatchFinally_Block.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Without_TryCatchFinally_Block {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a,b,c;
+        System.out.println("First No. : ");
+        a = sc.nextInt();
+        System.out.println("Second No. : ");
+        b = sc.nextInt();
+        c = a/b;
+        System.out.println("Result: "+ c);
+        System.out.println("Some other work");
+    }
+}
+
